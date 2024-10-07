f93b25aecc1927ee108bd921fecd276e7fa50d9e
Sam Urbaniak <spurbaniak@email.neit.edu>
2024-10-07 16:36:05 -0400

    js!

---
+++ week2/scripts/script.js
@@ -0,0 +1,9 @@
+var newmessage = "john cena is mad."
+
+var messagetag = document.getElementById("message");
+
+function showmessageonpage(){
+    messagetag.innerHTML = newmessage;
+}
+
+showmessageonpage();
